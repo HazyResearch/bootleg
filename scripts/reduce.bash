80776da0128e0b3b0740d027c9fb048976b33c20
rubi <rubimazaki@gmail.com>
2021-12-05 00:53:18 +0200

    removed junk line + fixed reduce to use environment variable

--- scripts/reduce.bash
+++ scripts/reduce.bash
@@ -1,8 +1,8 @@
 source ../venv/bin/activate
 source ./envs.bash
-rm -rf /nvme2/chatterbox/bootleg/output/full_wiki_0_-1_final/_temp
+rm -rf $BOOTLEG_DATA_DIR/output/full_wiki_0_-1_final/_temp
 python3 $BOOTLEG_CODE_DIR/bootleg/utils/preprocessing/sample_eval_data.py \
-    --data_dir /nvme2/chatterbox/bootleg/output/full_wiki_0_-1_final \
+    --data_dir $BOOTLEG_DATA_DIR/output/full_wiki_0_-1_final \
     --file dev.jsonl \
     --out_file_name dev_test_sample.jsonl \
     --sample_perc 0.1 \
